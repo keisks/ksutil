39ff58227189c3af8132ccf1f412c6a5eceba136
keisks <keisks5@gmail.com>
2013-04-26 15:14:52 +0900

    initial commit

---
+++ aclbib_crowl.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+wget -w 1 --user-agent="Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.0; Trident/5.0)" -r -N -np -A .bib http://aclweb.org/anthology-new/
+find . -name "*.bib" |xargs -i% cat % > acl_all.bib
