147adc54f38c246c6ead3b6d11a1d8ea80522c65
keisks <keisks5@gmail.com>
2018-05-13 23:45:11 -0400

    up

---
+++ cuda/check_cudnn_version.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+nvcc ./cudnn_version.cpp -o cudnn_version
+./cudnn_version
---
+++ cuda/cudnn_version.cpp
@@ -0,0 +1,7 @@
+#include <cudnn.h>
+#include <iostream>
+
+int main(int argc, char** argv) {
+    std::cout << "CUDNN_VERSION: " << CUDNN_VERSION << std::endl;
+    return 0;
+}
