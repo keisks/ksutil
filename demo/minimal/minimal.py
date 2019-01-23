6050391ea41d825f2001b89f6d08c69842f74e95
keisks <keisks5@gmail.com>
2019-01-23 21:32:47 +0000

    add logging function

--- demo/minimal/minimal.py
+++ demo/minimal/minimal.py
@@ -1,16 +1,20 @@
-#!/usr/bin/env python
-# -*- encoding: utf-8 -*-
+import os
+import sys
+import logging
 from flask import Flask, jsonify, render_template, request
 from scripts.answer import get_answer
 from scripts.cat_facts import get_cat_facts
 
 app = Flask(__name__)
+formatter = '%(levelname)s : %(asctime)s : %(message)s'
+logging.basicConfig(filename="./requests.log", level=logging.DEBUG, format=formatter)
  
 # default page 
 @app.route("/", methods=['GET', 'POST'])
 def run_index(): # this function name can be arbitrary.
     if request.method == "POST":
         food = request.form["food_submitted"]
+        logging.info("answered:\t {}".format(food))
         return "Oh, your favorite food is " + get_answer(food) + "!"
     return render_template("index.html", header="Minimal demo for NLP.")
 
@@ -20,9 +24,10 @@ def run_cat_facts(): # this function name can be arbitrary.
     if request.method == "GET":
         resp = get_cat_facts()
         text = resp["text"]
+        logging.info("cat facts:\t {}".format(text))
         return jsonify(result = text)
     return render_template("index.html", header="HEADER!")
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', debug=True)
 
