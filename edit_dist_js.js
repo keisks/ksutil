816d72d3371e6dbce40a1e3fa47ecf75f3751be1
keisks <keisks5@gmail.com>
2019-04-05 15:44:02 -0700

    0 should be int

--- edit_dist_js.js
+++ edit_dist_js.js
@@ -8,13 +8,13 @@ function levenshteinDistance(a, b) {
   b.unshift("#");
 
   var distanceMatrix = new Array(b.length).fill().map(() => Array(a.length).fill(0));
-  console.log(distanceMatrix);
+  //console.log(distanceMatrix);
 
   var edMatrix = new Array(b.length).fill().map(() => Array(a.length).fill(0));
   
   //initialize Matrices
 
-  distanceMatrix[0][0] = "0";
+  distanceMatrix[0][0] = 0;
   edMatrix[0][0] = "#";
 
   for (var i = 1; i < a.length; i++) {
@@ -52,14 +52,14 @@ function levenshteinDistance(a, b) {
     }
   }
 
+  //console.log(distanceMatrix);
+
   // get edits
   j = a.length-1;
   i = b.length-1;
   var prev_edit = edMatrix[i][j];
-  console.log(prev_edit);
   var edits = [prev_edit];
   while (prev_edit != "#") {
-    console.log(prev_edit);
     if (prev_edit == "I") {
       prev_edit = edMatrix[i][j-1];
       j = j-1 ;
@@ -86,5 +86,4 @@ function levenshteinDistance(a, b) {
   return [ed, edits]
 }
 
-
-console.log(levenshteinDistance("This is not a sentence.".split(" "), "This is a sentence.".split(" ")));
+console.log(levenshteinDistance("This isn't a long long sentence.".split(" "), "This is a sentence.".split(" ")));
