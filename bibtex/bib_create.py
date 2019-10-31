1922d6fa9f5d6453639c6dbf750dc813d2dda868
keisks <keisks5@gmail.com>
2019-10-31 09:59:47 -0700

    up

--- bibtex/bib_create.py
+++ bibtex/bib_create.py
@@ -8,13 +8,16 @@ import bibtexparser # pip install bibtexparser
 
 with open(sys.argv[1]) as bibtex_in:
     bib_database = bibtexparser.load(bibtex_in)
+    print("Original BibTeX entries are:")
     print(bib_database.entries[0])
     bib_info = copy.deepcopy(bib_database.entries[0])
     bib_info["Summary"] = "my summary"
     bib_info["Comments"] = "my comments"
-    print(bib_info)
+    #print(bib_info)
     bib_database.entries = [bib_info]
 
     with open("new_"+sys.argv[1], 'w') as bibtex_out:
         bibtexparser.dump(bib_database, bibtex_out)
+
+    print("new_" + sys.argv[1] + " is created.")
 
--- bibtex/readme.md
+++ bibtex/readme.md
@@ -1,11 +1,12 @@
 requirements:
     
     pip install bibtexparser
-    pip install pyinstaller (optional)
+    pip install pyinstaller (option)
 
 usage:
 
     python bib_create.py your_bibtex.bib
+    (option) pyinstaller bib_create.py, which builds build/bib_create/bib_create 
 
 this yields ``new_your_bibtex.bib''
 
