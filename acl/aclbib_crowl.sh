0fbbb93624d25d22cf62492628bfeb2c7ec8bcb9
Keisuke Sakaguchi <keisks5@gmail.com>
2025-08-28 09:14:14 +0900

    Delete acl directory

--- acl/aclbib_crowl.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# tiny script for crowling acl-anthology and create acl_all.bib.
-wget -w 1 --user-agent="Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.0; Trident/5.0)" -r -N -np -A .bib http://aclweb.org/anthology-new/
-find . -name "*.bib" |xargs -i% cat % > acl_all.bib
