75bd7b69a4ec5035aded998ba81e379d32531e71
Keisuke Sakaguchi <keisks5@gmail.com>
2025-09-03 09:43:14 +0900

    Delete cuda directory

--- cuda/check_cudnn_version.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-nvcc ./cudnn_version.cpp -o cudnn_version
-./cudnn_version
--- cuda/cudnn_version.cpp
+++
@@ -1,7 +0,0 @@
-#include <cudnn.h>
-#include <iostream>
-
-int main(int argc, char** argv) {
-    std::cout << "CUDNN_VERSION: " << CUDNN_VERSION << std::endl;
-    return 0;
-}
